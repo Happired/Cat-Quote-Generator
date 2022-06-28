6777ff971d64bbc31a55b51e5997f4d899f4cee5
Jocelyn Zheng <jocelynzheng@Jocelyns-Air.fios-router.home>
2022-06-28 17:32:11 -0400

    add css

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/index.html");
 })
@@ -27,37 +26,24 @@ app.post("/", function(req, res){
         response.on("data", function(data){
             var catData = JSON.parse(data);
 
+            // gets url for cat image from json response
             imageUrl = catData.file;
             console.log(imageUrl);
 
-
+            // randomly generates quote from internal json file
             var theQuote = quotes[Math.ceil(Math.random()*quotes.length)].text
 
-            res.send("<img src= "+imageUrl+" style = 'width: 200px; height: auto;'><br><p>"+theQuote+"</p><br><br><form action = '/' method = 'post'> <button type='submit'>Generate Cat</button></form>");
+            // sends the cat picture and quote back to client
+            // to do later: fix ugly html+css
+            res.send("<style> body{font-family: 'Roboto Mono', monospace;background-color: #DCD7C9;color: #2C3639;}button{font-family: 'Roboto Mono', monospace;,text-align:center;margin-left:40%;border:0px;background-color:#A27B5C;}</style><img src= "+imageUrl+" style = 'width: 200px; height: auto;'><br><p>"+theQuote+"</p><br><br><form action = '/' method = 'post'> <button type='submit'>Generate Cat</button></form>");
         })
         
     })} catch (e) {
+        // idk
         res.send('someone messed up lol')
     }
 
-    //Math.ceil(Math.random())*
-
-    /*var url = "https://api.quotable.io/random";
-    https.get(url, function(respon){
-        console.log(respon.statusCode);
-
-        respon.on("data", function(data){
-            var quoteData = JSON.parse(data);
-
-            quote = quoteData.content;
-            
-            
-        })
-    })*/
-
-
-
-    
+    //for debugging
     console.log(imageUrl);
     
 })
